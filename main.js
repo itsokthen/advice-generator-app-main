adcdaf494751ea6d20f7727c046e292cc87a3274
itsokthen <cades117@gmail.com>
2022-11-27 00:00:56 -0600

    finalized project

---
+++ main.js
@@ -0,0 +1,11 @@
+const fetchAdvice = async () => {
+  const results = await fetch("https://api.adviceslip.com/advice", {
+    cache: "no-cache",
+  });
+  const data = await results.json();
+
+  document.getElementById("title").innerHTML = `Advice #${data.slip.id};`;
+  document.getElementById("advice").innerHTML = ` "${data.slip.advice};"`;
+};
+
+fetchAdvice();
